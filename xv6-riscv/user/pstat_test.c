3df0db48ea023c1b5c1096fab56927cc93f77c7f
btnguyen <btnguyen@pyrite-n1.cs.iastate.edu>
2022-02-15 12:33:32 -0600

    uncommented test

--- xv6-riscv/user/pstat_test.c
+++ xv6-riscv/user/pstat_test.c
@@ -8,10 +8,10 @@ main(int argc, char *argv[]) {
   struct pstat stats;
   getpstat(&stats);
 
-  //for(int i =0; i < (sizeof(stats.pid) / sizeof(stats.pid[0])); i++)
-  //{
-   // printf("pid: %d\ninuse: %d\nnice value: %d\n", stats.pid[i], stats.inuse[i], stats.nice[i]);
-  //}  
+  for(int i =0; i < (sizeof(stats.pid) / sizeof(stats.pid[0])); i++)
+  {
+    printf("pid: %d\ninuse: %d\nnice value: %d\n", stats.pid[i], stats.inuse[i], stats.nice[i]);
+  }  
 
   return 0;
 }
